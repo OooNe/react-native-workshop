ca3816e92ee8d48033632a0015e40629527506dc
Marek Pekalski <mrk.pekalski@gmail.com>
2018-01-07 13:06:00 +0100

    Sample List

--- wtf-is-react/main.js
+++ wtf-is-react/main.js
@@ -1,7 +1,15 @@
 /*global React, ReactDOM */
 
+const languages = ['HTML5', 'Java', 'Ruby', 'JavaScript', 'PHP']
+
+function List(props) {
+    return React.createElement('ul', null, languages.map(function(language, index) {
+        return React.createElement('li', { key: index }, React.createElement('a', { href: `/${language}` }, language))
+    }))
+}
+
 function App(props) {
-    return React.createElement('div', null, `Hello ${props.toWhat}`)
+    return React.createElement('div', null, React.createElement(List))
 }
 
 ReactDOM.render(
