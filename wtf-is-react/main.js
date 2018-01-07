804dfa7f1d2139c9d077510ab117fcee956650b5
Marek Pekalski <mrk.pekalski@gmail.com>
2018-01-07 13:25:42 +0100

    Props && State

--- wtf-is-react/main.js
+++ wtf-is-react/main.js
@@ -1,15 +1,48 @@
 /*global React, ReactDOM */
 
-const languages = ['HTML5', 'Java', 'Ruby', 'JavaScript', 'PHP']
-
 function List(props) {
-    return React.createElement('ul', null, languages.map(function(language, index) {
+    const languagesToShow = props.languages.filter(function (language) {
+        return language.includes(props.filter)
+    })
+
+    return React.createElement('ul', null, languagesToShow.map(function(language, index) {
         return React.createElement('li', { key: index }, React.createElement('a', { href: `/${language}` }, language))
     }))
 }
 
-function App(props) {
-    return React.createElement('div', null, React.createElement(List))
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+
+        const languages = ['HTML5', 'Java', 'Ruby', 'JavaScript', 'PHP']
+
+        this.state = {
+            languages: languages,
+            filter: ''
+        }
+
+        this.handleFilter = this.handleFilter.bind(this)
+    }
+
+    handleFilter(e) {
+        const inputValue = e.target.value
+
+        this.setState(function () {
+            return {
+                filter: inputValue
+            }
+        })
+    }
+
+    render() {
+        return React.createElement('div', null,
+            [
+                React.createElement('h1', null, 'Sample App'),
+                React.createElement('input', { onChange: this.handleFilter }),
+                React.createElement(List, { languages: this.state.languages, filter: this.state.filter })
+            ]
+        )
+    }
 }
 
 ReactDOM.render(
